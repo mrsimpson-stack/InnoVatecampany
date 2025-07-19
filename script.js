a60b642bf2fbcb163ee3d7ea1d5dc4a614997bbf
mrsimpson-stack <simpsontechnology7@gmail.com>
2025-07-19 21:37:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ function login() {
         id: 'user123',
         name: 'Active User',
         phone: phone,
-        balance: 7000
+        balance: 9000
     };
 
     // Save token to localStorage
